bf01db4df678a1b54ca077302e7625164b7a7fb1
manasi-marine-management <93929888+manasi-marine-management@users.noreply.github.com>
2021-11-21 20:05:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll",function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 0)
+  })
+  
+  var hamburger = document.querySelector(".hamburger");
+  var Menudiv = document.querySelector(".Menu-div");
+  
+  hamburger.addEventListener("click",responsive);
+  function responsive() {
+    hamburger.classList.toggle("active");
+    Menudiv.classList.toggle("active");
+  }
+  
+  const link = document.querySelectorAll(".item");
+  link.forEach(n => n.addEventListener("click",closeMenu));
+  function closeMenu(){
+    hamburger.classList.remove("active");
+    Menudiv.classList.remove("active");
+  }
+
+
+  
